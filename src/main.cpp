efb45c5ce18a82aac6f50095c73e8b27ee9ae752
KnightLeo <knightleo602@gmail.com>
2022-08-09 14:31:30 -0300

    coisas necessarias pra entrega
    - nome
    - enunciado
    - mais uns ajustes nos comentarios

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,42 @@
+// Leonardo Matthew Knight
+
+/*
+ENUNCIADO
+
+Para  obter  os  pontos  relativos  a  este  trabalho,  você  deverá  criar  um  programa,  utilizando  a
+linguagem  Python, C, ou C++.  Este  programa,  quando  executado,  irá  determinar  se  uma  string de
+entrada  faz  parte  da  linguagem  𝐿    definida  por  𝐿 = {𝑥 | 𝑥 ∈ {𝑎,𝑏}∗ 𝑒 𝑐𝑎𝑑𝑎 𝑎 𝑒𝑚 𝑥 é
+𝑠𝑒𝑔𝑢𝑖𝑑𝑜 𝑝𝑜𝑟 𝑝𝑒𝑙𝑜 𝑚𝑒𝑛𝑜𝑠 𝑑𝑜𝑖𝑠 𝑏} segundo o alfabeto  Σ={𝑎,𝑏,𝑐}.
+
+O  programa  que  você  desenvolverá  irá  receber  como  entrada um arquivo de texto  (.txt)
+contendo várias strings. A primeira linha do arquivo indica quantas strings estão no arquivo de texto de
+entrada. As linhas subsequentes contém uma string por linha.  A seguir está um exemplo das linhas que
+podem existir em um arquivo de testes para o programa que você irá desenvolver:
+
+3
+abbaba
+abababb
+bbabbaaab
+
+
+Neste  exemplo  temos  3  strings  de  entrada.  O  número  de  strings em  cada  arquivo  será
+representado  por  um  número  inteiro  positivo.  A  resposta  do  seu  programa  deverá  conter  uma, e
+somente uma linha de saída para cada string. Estas linhas conterão a string de entrada e o resultado
+da validação conforme o formato indicado a seguir:
+
+abbaba: não pertence.
+
+
+A  saída  poderá  ser  enviada  para  um  arquivo  de  textos,  ou  para  o  terminal  padrão  e  será
+composta de uma linha de saída por string de entrada. No caso do exemplo, teremos 3 linhas de saída.
+
+Para que seu programa possa ser testado você deve criar, no mínimo, três arquivos de entrada
+contendo um número diferente de strings diferentes. Os arquivos de entrada criados para os seus testes
+devem estar disponíveis tanto no ambiente repl.it quanto no ambiente Github. Observe que o professor
+irá  testar  seu  programa  com  os  arquivos  de  testes  que  você  criar  e  com,  no  mínimo  um  arquivo  de
+testes criado pelo próprio professor.
+ */
+
 #include <iostream>
 #include <fstream>
 #include <stdbool.h>
@@ -12,16 +51,16 @@ enum class states
 	s2 = 2
 };
 
-states &operator++(states &s)
+states &operator++(states &s) // when B is received
 {
 	if (s == states::s2 || s == states::sn1)
 		return s = states::s2; // returns s2 when on last or first state
-	return s = static_cast<states>(static_cast<int>(s) + 1);
+	return s = static_cast<states>(static_cast<int>(s) + 1); // else, returns the next state
 }
 
-states &operator--(states &s)
+states &operator--(states &s) // when A is received
 {
-	return s = states::s0; // returns s0 whenever -- (A) is received
+	return s = states::s0; // returns s0 by default
 }
 
 bool contains(char array[], char value)
@@ -50,6 +89,7 @@ int main()
 
 	ifstream input;
 	ofstream output;
+
 	if (choice == 4)
 	{
 		/*
@@ -69,6 +109,7 @@ int main()
 	{
 		input.open("inputs-output/input" + to_string(choice) + ".txt");
 	}
+
 	if (!input)
 	{
 		// cout << "Could not find file or selected option doesn't exist!" << endl;
